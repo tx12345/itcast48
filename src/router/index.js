449c5eafcbfd77ad2f57357ba2a814b412e02400
wangbo <516759183@qq.com>
2018-08-09 19:33:51 +0800

    login--elment-ui配置

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,13 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Vue from 'vue';
+import Router from 'vue-router';
+import Login from '@/views/login';
+// 在components文件中删除了helloworld.vue文件，此行代码可以删除了
+// import HelloWorld from '@/components/HelloWorld'
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
+    { name : 'login', path : '/login',component : Login}
   ]
 })
---
+++ src/views/login.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="login">
+    <el-form :label-position="labelPosition" label-width="80px" :model="formData">
+      <el-form-item label="用户名">
+        <el-input v-model="formData.username"></el-input>
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input v-model="formData.password"></el-input>
+      </el-form-item>
+      <el-form-item label="活动形式">
+        <el-button type="primary">登录</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+<script>
+  export default {
+    data() {
+      return {
+        labelPosition: 'top',
+        formData: {
+          username: '',
+          password: ''
+        }
+      };
+    }
+  }
+</script>
+<style type="text/css">
+
+</style>
