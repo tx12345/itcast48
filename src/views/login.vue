fe502d4698ad8305816d8302f14edbae210e55e2
wangbo <516759183@qq.com>
2018-08-09 21:12:51 +0800

    longin--请求登录完成

--- src/views/login.vue
+++ src/views/login.vue
@@ -6,15 +6,17 @@
         <el-input v-model="formData.username"></el-input>
       </el-form-item>
       <el-form-item label="密码">
-        <el-input v-model="formData.password"></el-input>
+        <el-input  type="password" v-model="formData.password"></el-input>
       </el-form-item>
       <el-form-item>
-        <el-button class="loginBtn"type="primary">登录</el-button>
+          <!-- <el-button :plain="true" @click="open2">成功</el-button> -->
+        <el-button class="loginBtn" type="primary" @click="handleLogin">登录</el-button>
       </el-form-item>
     </el-form>
   </div>
 </template>
 <script>
+  import axios from 'axios';
   export default {
     data() {
       return {
@@ -24,10 +26,35 @@
           password: ''
         }
       };
+    },
+    methods : {
+      handleLogin (){
+        axios.post('http://localhost:8888/api/private/v1/login',this.formData)
+              .then(response => {
+                console.log(response)
+                var { data : { meta : { status, msg }}} = response; //解构赋值
+                console.log(status)
+                console.log(msg)
+                if(response.status === 200){
+                  // 登录成功信息提示
+                  this.$message.success(msg);//来自element-ui
+                  // 记录token
+                  var token = response.data.data.token;
+                  //存放token到sessionStorage中
+                  sessionStorage.setItem('token',token);
+                  //跳转到后台首页users
+                }else {
+                  this.$message.error(msg)
+                }
+              })
+              .catch(err => {
+                console.log(err)
+              })
+      }
     }
   }
 </script>
-<style type="text/css">
+<style scoped>
   .loginWrap {
     background-color: #324152;
     height: 100%;
